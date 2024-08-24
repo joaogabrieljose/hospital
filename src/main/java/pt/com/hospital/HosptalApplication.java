83498874a07d72590c361d9c9148068649f3b034
João José <joaogjose@outlook.com>
2024-08-24 01:51:39 +0100

    primeiro commite config

---
+++ src/main/java/pt/com/hospital/HosptalApplication.java
@@ -0,0 +1,13 @@
+package pt.com.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosptalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HosptalApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pt/com/hospital/domain/domain.java
@@ -0,0 +1,4 @@
+package pt.com.hospital.domain;
+
+public class domain {
+}
